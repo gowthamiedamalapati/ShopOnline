26ce7089b1a70c01416ea8c220c415e24990ce0c
gowthamiedamalapati <gouthami1812@gmail.com>
2021-03-15 11:17:53 +0530

    User Login, Register Form UI

--- Shoponline/src/app/app.module.ts
+++ Shoponline/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBRYjSgG1JuxS-BlSYMHrhnP_8rW_E9lsc",
+      authDomain: "shoponline-1998b.firebaseapp.com",
+      projectId: "shoponline-1998b",
+      storageBucket: "shoponline-1998b.appspot.com",
+      messagingSenderId: "439392781775",
+      appId: "1:439392781775:web:8cc36186fe01f884893eba"
+    }),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Shoponline/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  username:string="";
+  email="";
+  password="";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  register(formValue:NgForm){
+    alert("Successful");
+  }
+
+}
